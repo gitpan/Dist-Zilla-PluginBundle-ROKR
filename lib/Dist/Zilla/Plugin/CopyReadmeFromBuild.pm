a0fe185906f3742f3d43c31d0ca7299feffa0bb2
Robert Krimen <rokr@cpan.org>
2010-05-12 16:38:55 +0000

    Import of ROKR/Dist-Zilla-PluginBundle-ROKR-0.0010 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-PluginBundle-ROKR
    gitpan-cpan-version:      0.0010
    gitpan-cpan-path:         ROKR/Dist-Zilla-PluginBundle-ROKR-0.0010.tar.gz
    gitpan-cpan-author:       ROKR
    gitpan-cpan-maturity:     released

---
+++ lib/Dist/Zilla/Plugin/CopyReadmeFromBuild.pm
@@ -0,0 +1,74 @@
+package Dist::Zilla::Plugin::CopyReadmeFromBuild;
+BEGIN {
+  $Dist::Zilla::Plugin::CopyReadmeFromBuild::VERSION = '0.0010';
+}
+# ABSTRACT: Copy README after building (for SCM inclusion, etc.)
+
+
+use Moose;
+with qw/ Dist::Zilla::Role::AfterBuild /;
+
+use File::Copy qw/ copy /;
+
+sub after_build {
+    my $self = shift;
+    my $data = shift;
+
+    my $build_root = $data->{build_root};
+    my $src;
+    for(qw/ README README.md README.txt README.markdown /) {
+        my $file = $build_root->file( $_ );
+        $src = $file and last if -e $file;
+    }
+
+    die "Missing README file in ", $build_root unless $src;
+
+    my $dest = $self->zilla->root->file( $src->basename );
+
+    copy "$src", "$dest" or die "Unable to copy $src to $dest: $!";
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Dist::Zilla::Plugin::CopyReadmeFromBuild - Copy README after building (for SCM inclusion, etc.)
+
+=head1 VERSION
+
+version 0.0010
+
+=head1 SYNOPSIS
+
+In your L<Dist::Zilla> C<dist.ini>:
+
+    [CopyReadmeFromBuild]
+
+=head1 DESCRIPTION
+
+CopyReadmeFromBuild will automatically copy the README from the build directory into the distribution directoory. This is so you can commit the README to version control. GitHub, for example, likes to see a README
+
+Dist::Zilla::Plugin::Readme will not like it if you already have a README, so you'll have to disable that plugin, an example of which is:
+
+    [@Filter]
+    bundle = @Basic
+    remove = Readme
+
+=head1 AUTHOR
+
+  Robert Krimen <robertkrimen@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Robert Krimen.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
---
+++ lib/Dist/Zilla/PluginBundle/ROKR/Basic.pm
@@ -0,0 +1,109 @@
+package Dist::Zilla::PluginBundle::ROKR::Basic;
+BEGIN {
+  $Dist::Zilla::PluginBundle::ROKR::Basic::VERSION = '0.0010';
+}
+# ABSTRACT: ROKR::Basic PluginBundle for Dist::Zilla
+
+
+use Moose;
+
+with qw/ Dist::Zilla::Role::PluginBundle /;
+
+use Dist::Zilla::PluginBundle::Basic;
+use Dist::Zilla::PluginBundle::Filter;
+
+sub bundle_config {
+    my ($self, $section) = @_;
+
+    my @bundle = Dist::Zilla::PluginBundle::Filter->bundle_config({
+        name    => $section->{name} . '/@Basic',
+        payload => {
+            bundle => '@Basic',
+            remove => [
+                # We'll generate README from ReadmeFromPod 
+                'Readme',
+            ],
+        },
+    });
+
+    push @bundle, map {
+        my ( $name, $payload ) = @$_;
+        [ "$section->{name}/$name" => "Dist::Zilla::Plugin::$name" => $payload ];
+    } (
+        [ 'CopyReadmeFromBuild' ],
+        [ 'DynamicManifest' ],
+        [ 'ReadmeFromPod' ],
+        [ 'SurgicalPkgVersion' ],
+        [ 'SurgicalPodWeaver' ],
+    );
+
+    return @bundle;
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Dist::Zilla::PluginBundle::ROKR::Basic - ROKR::Basic PluginBundle for Dist::Zilla
+
+=head1 VERSION
+
+version 0.0010
+
+=head1 SYNOPSIS
+
+In your L<Dist::Zilla> C<dist.ini>:
+
+    [@ROKR::Basic]
+
+=head1 DESCRIPTION
+
+This is an enhancement on the @Basic bundle (L<Dist::Zilla::PluginBundle::Basic>) with the following:
+
+    @Basic (without Readme)
+    CopyReadmeFromBuild
+    DynamicManifest
+    SurgicalPkgVersion
+    SurgicalPodWeaver
+
+It is equivalent to:
+
+    [@Filter]
+    bundle = @Basic
+    remove = Readme
+
+    [CopyReadmeFromBuild]
+    [DynamicManifest]
+    [SurgicalPkgVersion]
+    [SurgicalPodWeaver]
+
+=head1 SEE ALSO
+
+L<Dist::Zilla::PluginBundle::Basic>
+
+L<Dist::Zilla::Plugin::CopyReadmeFromBuild>
+
+L<Dist::Zilla::Plugin::DynamicManifest>
+
+L<Dist::Zilla::Plugin::SurgicalPkgVersion>
+
+L<Dist::Zilla::Plugin::SurgicalPodWeaver>
+
+=head1 AUTHOR
+
+  Robert Krimen <robertkrimen@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Robert Krimen.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
